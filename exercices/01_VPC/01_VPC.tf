148564b8a88a8208120f010cb4fc63ab3f42788d
Alexandre Parès <alexandre@pares.pw>
2020-02-07 15:33:22 +0100

    add TP1 TF terraform

--- exercices/01_VPC/01_VPC.tf
+++ exercices/01_VPC/01_VPC.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
+
 resource "aws_vpc" "vpc_tp" {
   cidr_block = "172.16.0.0/16"
   tags = {
@@ -10,6 +11,7 @@ resource "aws_vpc" "vpc_tp" {
   }
   enable_dns_hostnames = true
 }
+
 
 resource "aws_subnet" "sub_pub" {
   vpc_id     = aws_vpc.vpc_tp.id
@@ -20,6 +22,7 @@ resource "aws_subnet" "sub_pub" {
   }
 }
 
+
 resource "aws_subnet" "sub_priv" {
   vpc_id     = aws_vpc.vpc_tp.id
   cidr_block = "172.16.2.0/24"
@@ -29,6 +32,7 @@ resource "aws_subnet" "sub_priv" {
   }
 }
 
+
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.vpc_tp.id
 
@@ -36,6 +40,7 @@ resource "aws_internet_gateway" "gw" {
     Name = "TP_CICD_IGW"
   }
 }
+
 
 resource "aws_route_table" "r" {
   vpc_id = aws_vpc.vpc_tp.id
@@ -49,3 +54,36 @@ resource "aws_route_table" "r" {
 }
 
 
+resource "aws_security_group" "Nat_Jump" {
+  name        = "Nat_Jump"
+  description = "Allow All inbound traffic from EFREI network"
+  vpc_id      = aws_vpc.vpc_tp.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.102.224.0/24"] # EFREI Public IPs see https://bgp.he.net/net/192.102.224.0/24
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+
+resource "aws_instance" "ubuntu" {
+  ami = "ami-035966e8adab4aaad"
+  instance_type = "t2.micro"
+
+  associate_public_ip_address = true
+
+  subnet_id = aws_subnet.sub_pub.id
+  user_data = file("01_EC2_user-data.sh")
+
+  tags = {
+    Name = "Nat_JumpHost"
+  }
+
+  vpc_security_group_ids = [aws_security_group.Nat_Jump.id]
+}
