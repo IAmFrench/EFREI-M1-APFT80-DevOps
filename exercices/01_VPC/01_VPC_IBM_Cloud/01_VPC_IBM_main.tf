56f927a2b8147080aa1d22b7b77c518aa49b2f00
Alexandre Parès <alexandre@pares.pw>
2020-02-11 20:09:45 +0100

    Add subnets

--- exercices/01_VPC/01_VPC_IBM_Cloud/01_VPC_IBM_main.tf
+++ exercices/01_VPC/01_VPC_IBM_Cloud/01_VPC_IBM_main.tf
@@ -8,36 +8,19 @@ variable "tags" {
 }
 
 # Create a VPC inside the Training resource group
-resource "ibm_is_vpc" "PF_ITOPS" {
-  name = "pf-itops"
+resource "ibm_is_vpc" "PF_ITOPS_vpc" {
+  name = "pf-itops-vpc"
   resource_group = data.ibm_resource_group.Training.id
   tags = var.tags
 }
 
-resource "ibm_is_network_acl" "defaultACL" {
-    vpc = ibm_is_vpc.PF_ITOPS.id
-    name = "pf-itops-defaultACL"
-    rules {
-        name        = "outbound"
-        action      = "allow"
-        source      = "0.0.0.0/0"
-        destination = "0.0.0.0/0"
-        direction   = "outbound"
-        icmp {
-          code = 1
-          type = 1
-        }
-    }
-    rules {
-        name        = "inbound"
-        action      = "deny"
-        source      = "0.0.0.0/0"
-        destination = "0.0.0.0/0"
-        direction   = "inbound"
-        icmp {
-          code = 1
-          type = 1
-        }
-    }
+variable "zone" {
+  type = string
 }
 
+resource "ibm_is_subnet" "sub_a" {
+  name            = "pf-itops-sub-a"
+  vpc             = ibm_is_vpc.PF_ITOPS_vpc.id
+  zone            = var.zone
+  ipv4_cidr_block = "192.168.0.0/24"
+}
--- exercices/01_VPC/01_VPC_IBM_Cloud/terraform.tfvars
+++ exercices/01_VPC/01_VPC_IBM_Cloud/terraform.tfvars
@@ -1,3 +1,3 @@
-zone       = ""
+zone       = "us-south" # Dallas or "us-east" for Washington DC
 pubKeyPath = "../01_rufol.pub"
 tags       = ["env:dev", "entity:PF/IT-OPS"]
